f04d8518117eaf08a789fece2b12dc38481fb21f
marthaberner <marthaberner.mb@gmail.com>
2016-03-01 16:59:38 -0700

    add tests

--- test/bubble.test.js
+++ test/bubble.test.js
@@ -1,13 +1,32 @@
 var assert = require('assert');
 var bubbleSort = require('../lib/bubble');
 
+function checkResults(array, expected) {
+  return array.length === 0 ? [false] : array.filter(function (num, i) {
+    return expected[i] !== num;
+  })
+}
+
 describe('#bubbleSort', function() {
   it('swaps two numbers', function () {
     var input = [5, 2];
     var expected = [2, 5];
-    var sorted = bubbleSort(input);
-    assert.equal(sorted.length, expected.length)
-    assert.equal(sorted[0], expected[0]);
-    assert.equal(sorted[1], expected[1]);
+    var sortResults = bubbleSort(input);
+    var checkedResults = checkResults(sortResults, expected).length
+    assert.equal(checkedResults, 0);
+  });
+  it('sorts an array of three', function () {
+    var input = [5, 2, 1];
+    var expected = [1, 2, 5];
+    var sortResults = bubbleSort(input);
+    var checkedResults = checkResults(sortResults, expected).length
+    assert.equal(checkedResults, 0);
+  });
+  it('sorts an array of 8', function () {
+    var input = [5, 2, 4, 3, 8, 10, 1, 0];
+    var expected = [0, 1, 2, 3, 4, 5, 8, 10];
+    var sortResults = bubbleSort(input);
+    var checkedResults = checkResults(sortResults, expected).length
+    assert.equal(checkedResults, 0);
   });
 });
