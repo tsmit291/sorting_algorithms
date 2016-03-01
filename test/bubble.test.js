66a0341f077e6aecfabe813b5054977b1171a344
marthaberner <marthaberner.mb@gmail.com>
2016-02-29 17:56:47 -0700

    add initial test

--- test/bubble.test.js
+++ test/bubble.test.js
@@ -1,8 +1,13 @@
 var assert = require('assert');
 var bubbleSort = require('../lib/bubble');
 
-describe('Bubble', function() {
-  it('wired up', function () {
-    assert.equal(bubbleSort(), 'hello');
+describe('#bubbleSort', function() {
+  it('swaps two numbers', function () {
+    var input = [5, 2];
+    var expected = [2, 5];
+    var sorted = bubbleSort(input);
+    assert.equal(sorted.length, expected.length)
+    assert.equal(sorted[0], expected[0]);
+    assert.equal(sorted[1], expected[1]);
   });
 });
