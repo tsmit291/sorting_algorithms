58fac27bca8bf69449fb5344771e483a23d12e29
marthaberner <marthaberner.mb@gmail.com>
2016-03-21 16:17:20 -0600

    selection sort set up :

---
+++ test/selection_sort.test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var selectionSort = require('../lib/selection_sort');
+
+describe('#selectionSort', function() {
+  it('wired up', function () {
+    assert.equal(0, 0);
+  });
+});
