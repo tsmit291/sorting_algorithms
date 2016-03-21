92fb864df2672675fef9c24ed992138ff690c19e
marthaberner <marthaberner.mb@gmail.com>
2016-03-21 16:57:14 -0600

    select sort tests

--- test/selection_sort.test.js
+++ test/selection_sort.test.js
@@ -1,8 +1,25 @@
 var assert = require('assert');
-var selectionSort = require('../lib/selection_sort');
+var selectSort = require('../lib/selection_sort');
 
-describe('#selectionSort', function() {
-  it('wired up', function () {
-    assert.equal(0, 0);
+describe('#selectSort', function() {
+  describe('#swap', function () {
+    it('swaps two numbers', function () {
+      var input = [5, 2];
+      var expected = [2, 5];
+      var sortResults = selectSort.sort(input);
+      assert.deepEqual(sortResults, expected);
+    });
+  })
+  xit('sorts an array of three', function () {
+    var input = [5, 2, 1];
+    var expected = [1, 2, 5];
+    var sortResults = selectSort.sort(input);
+    assert.deepEqual(sortResults, expected);
+  });
+  xit('sorts an array of 8', function () {
+    var input = [5, 2, 4, 3, 8, 10, 1, 0];
+    var expected = [0, 1, 2, 3, 4, 5, 8, 10];
+    var sortResults = selectSort.sort(input);
+    assert.deepEqual(sortResults, expected);
   });
 });
