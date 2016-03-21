e9550ec41cf71096582ab1a781fa139889daac6d
marthaberner <marthaberner.mb@gmail.com>
2016-03-21 16:49:40 -0600

    update tests for deep equal

--- test/bubble_sort.test.js
+++ test/bubble_sort.test.js
@@ -1,32 +1,23 @@
 var assert = require('assert');
 var bubbleSort = require('../lib/bubble_sort');
 
-function checkResults(array, expected) {
-  return array.length === 0 ? [false] : array.filter(function (num, i) {
-    return expected[i] !== num;
-  })
-}
-
 describe('#bubbleSort', function() {
   it('swaps two numbers', function () {
     var input = [5, 2];
     var expected = [2, 5];
     var sortResults = bubbleSort(input);
-    var checkedResults = checkResults(sortResults, expected).length
-    assert.equal(checkedResults, 0);
+    assert.deepEqual(sortResults, expected);
   });
   xit('sorts an array of three', function () {
     var input = [5, 2, 1];
     var expected = [1, 2, 5];
     var sortResults = bubbleSort(input);
-    var checkedResults = checkResults(sortResults, expected).length
-    assert.equal(checkedResults, 0);
+    assert.deepEqual(sortResults, expected);
   });
   xit('sorts an array of 8', function () {
     var input = [5, 2, 4, 3, 8, 10, 1, 0];
     var expected = [0, 1, 2, 3, 4, 5, 8, 10];
     var sortResults = bubbleSort(input);
-    var checkedResults = checkResults(sortResults, expected).length
-    assert.equal(checkedResults, 0);
+    assert.deepEqual(sortResults, expected);
   });
 });
